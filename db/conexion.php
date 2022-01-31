8caf22b63228f3c288d5d888e95ca2585af9d027
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2022-01-31 17:51:10 -0500

    fix error with utf

--- db/conexion.php
+++ db/conexion.php
@@ -3,6 +3,7 @@
 include_once 'credentials.php';
 
 $conexion = $mysqli = new mysqli($host, $user, $pass, $db);
+$conexion->set_charset("utf8");
 
 if (mysqli_connect_errno()) {
 	echo 'Conexion Fallida : ', mysqli_connect_error();
